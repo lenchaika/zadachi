fb151bb2abcfeb20415a4cc1cdb03b5cee7070b3
Lena <len.chaika2011@yandex.ru>
2022-12-30 01:55:30 +0300

    seminar

--- seminar/Program.cs
+++ seminar/Program.cs
@@ -1,4 +1,6 @@
 ﻿// дз
+
+
 Main();
 
 void Main()
@@ -17,6 +19,10 @@ void Main()
             case "Task19": Task19(); break;
             case "Task25": Task25(); break;
             case "Task29": Task29(); break;
+            case "Task28": Task28(); break;
+            case "TaskN": TaskN(); break;
+            case "seminar4": seminar4(); break;
+            case "Task30": Task30(); break;
             case "exit": isWorking = false; break;
         }
         Console.WriteLine();
@@ -125,6 +131,60 @@ void Task29()
 }
 
 
+
+void seminar4()//задача 24 - напишите программу которая принимает на вход число А и выдает сумму числе от 1 до А
+{
+    Console.WriteLine("seminar4");
+    int number = ReadInt ("number");
+
+    Console.WriteLine(GetSumUntil(number));
+
+}
+
+void TaskN()// программа принимает на вход число и выдает количество цифр // через математичексие операторы
+{
+    Console.WriteLine("TaskN");
+    int number = ReadInt("number");
+    int count = 0;
+    while(number > 0)
+    {
+        number /=10;
+        count++;
+    }
+    Console.WriteLine(count);
+}
+
+void Task28()// программа принимает на вход число N и выдаем произведение чисел от 1 до N - факториал
+{
+    Console.WriteLine("Task28");
+    int number = ReadInt("number");
+    Console.WriteLine(factorial(number));
+}
+
+
+void Task30()// выводит массив из 8 элементов заплненый нулями и единицами  в случайном порядке
+{
+    Console.WriteLine("Task30");
+    
+    int[] array = GetArray(ReadInt("array length"));
+    
+    Console.WriteLine(ArrayToString(array));
+
+}
+
+int [] GetArray (int length)// функция создания массива
+{
+    int [] array = new int[8];  // создали массив
+
+    //заполняет пользователь массив
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+
+    return array;
+} 
+
 int FindMax(int firstNumber, int secondNumber, int thirdNumber)
 {
     int max = firstNumber;
@@ -143,14 +203,14 @@ int FindMax(int firstNumber, int secondNumber, int thirdNumber)
 
 
 
-int ReadInt(string argumentName)
+int ReadInt(string argumentName)// функция ввода числа
 {
     Console.Write($"Input {argumentName}: ");
-    return int.Parse(Console.ReadLine());
+    return int.Parse(Console.ReadLine()); // возвращает от пользователя
 }
-bool IsLargeThen(int firstNumber, int secindNumber)
+bool IsLargeThen(int firstNumber, int secondNumber) // bool -тру или фолз принимает
 {
-    return firstNumber > secindNumber;
+    return firstNumber > secondNumber; // возвращает результат сравнения
 }
 
 bool IsEven(int number)
@@ -180,7 +240,7 @@ int GetDigitFromNumber(int number, int digitPosition)
      digitPosition].ToString());
 }
 
-int Pow(int firstNumber, int secondNumber)
+int Pow(int firstNumber, int secondNumber)// функция возведения в степень
 {
     int result = 1;
     for (int i = 0;  i < secondNumber; i++)
@@ -190,18 +250,7 @@ int Pow(int firstNumber, int secondNumber)
     return result;
 }
 
-int [] GetArray (int length)// функция создания массива
-{
-    int [] array = new int[length];  // создали массив
 
-    //заполняет пользователь массив
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = ReadInt($"element {i}");
-    }
-
-    return array;
-} 
 //массив вывод на экран
 string ArrayToString(int[] array)
 {
@@ -213,3 +262,26 @@ string ArrayToString(int[] array)
     return result;
 }
 
+ int GetSumUntil(int n) // функция суммы 
+ {
+    int result = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        result += i;
+    }
+    return result;
+ }
+
+
+
+int factorial(int n)
+{
+    int sum = 1;
+    for (int i = 1; i <= n; i++)
+
+    {
+        sum *= i;
+    }
+   return sum;
+}

9c94c136f06c092b49786dfa460762f892543241
Lena <len.chaika2011@yandex.ru>
2023-01-01 23:52:05 +0300

    zadacha35

---
+++ zadacha35/Program.cs
@@ -0,0 +1,53 @@
+﻿// задайте одномерный массив  из 123 случайных чисел
+// найдите количество элементов массива значение которых в отрезке [10,99]
+
+//int length = ReadInt("Array length");
+//int find = ReadInt("Find number");
+int[] array = GetRandomArray(123, 0, 1000);
+PrintArray(array);
+Console.WriteLine();
+int count = FindElements(array);
+Console.WriteLine(count);
+
+int ReadInt(string argument)
+{
+    Console.Write($"Input {argument}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+
+int[] GetRandomArray(int length, int minValue, int maxValue)// новый масси длинной ленгс
+{
+    int [] array = new int[length];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+
+
+int FindElements(int[]array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+     {
+        if ((array[i] >= 10) && (array[i] <= 99))
+        {
+                count++;
+        }
+       
+    }
+     return count;
+}
---
+++ zadacha37/Program.cs
@@ -0,0 +1,48 @@
+﻿// найти произведение  пар чисел в одномерном массиве . парой считаем первый и последний элемент ,второй и предпоследний и т.д.
+//результат в новом массиве
+
+
+//int length = ReadInt("Array length");
+//int find = ReadInt("Find number");
+int[] array = GetRandomArray(9, 0, 50);
+PrintArray(array);
+Console.WriteLine();
+int[] newarray = NewArray(array);
+PrintArray(newarray);
+
+
+
+int[] GetRandomArray(int length, int minValue, int maxValue)// новый массив длинной ленгс
+{
+    int [] array = new int[length];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+
+
+int [] NewArray(int[] array)
+
+{
+    int [] newarray = new int[(array.Length / 2) + 1];
+    for (int i = 0; i < array.Length/2; i++)
+    {
+         newarray[i] = array[i] * array[array.Length - 1 - i];
+    }
+    if (array.Length % 2 != 0)
+    newarray[(array.Length/2) - 1] = array[array.Length/2];
+    return newarray;
+}

58ba2f0fdaccd5d8280fbd2056b7593a6efe27e7
Lena <len.chaika2011@yandex.ru>
2023-01-01 21:34:10 +0300

    zadacha32

---
+++ zadacha32/Program.cs
@@ -0,0 +1,49 @@
+﻿// напишите  программу  замена  элементов массива положительные элементы замените на соответсвующие  отрицательные и наоборот
+
+
+int length = ReadInt("Array length");
+int[] array = GetRandomArray(length, -15, 15);
+PrintArray(array);
+Console.WriteLine();
+ReversArray(array);
+PrintArray(array);
+
+int ReadInt(string argument)
+{
+    Console.Write($"Input {argument}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+int[] GetRandomArray(int length, int minValue, int maxValue)// новый масси длинной ленгс
+{
+    int [] array = new int[length];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+
+void ReversArray(int[]array)
+{
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        {
+            array[i] = -array[i];
+        }
+        
+    }
+   
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
---
+++ zadacha33/Program.cs
@@ -0,0 +1,66 @@
+﻿// задайте массив.напишите программу которая определяет присутствует  ли заданное число в массиве.
+// 4 массив [6,7,19,345,3] нет
+
+
+int length = ReadInt("Array length");
+int find = ReadInt("Find number");
+int[] array = GetRandomArray(length, -15, 15);
+PrintArray(array);
+Console.WriteLine();
+FindNumber(array, find);
+
+
+int ReadInt(string argument)
+{
+    Console.Write($"Input {argument}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+int[] GetRandomArray(int length, int minValue, int maxValue)// новый масси длинной ленгс
+{
+    int [] array = new int[length];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+
+void ReversArray(int[]array)
+{
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        {
+            array[i] = -array[i];
+        }
+        
+    }
+   
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+void FindNumber(int[] array, int findNumber)
+{
+    for (int i = 0; i < array.Length; i++)
+
+    {
+        if (array[i] == find)
+        {
+            Console.WriteLine("Yes");
+            return;
+        }
+        
+    }
+    Console.WriteLine("No");
+}

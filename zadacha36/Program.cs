212a7bbba7d6a82c0d218f9c8d00beac8fca72c7
Lena <len.chaika2011@yandex.ru>
2023-01-02 15:49:16 +0300

    zadacha36

---
+++ zadacha36/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+
+
+int length = ReadInt("Array length");
+int[]array = GetRandomArray(length, 1, 100);
+int result = 0;
+FindSum(array);
+PrintArray(array);
+
+
+int ReadInt(string argument)
+{
+    Console.Write($"Input{argument}");
+    return int.Parse(Console.ReadLine());
+}
+
+int[] GetRandomArray(int length, int minValue, int maxValue)
+{
+    int [] array = new int[length];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+
+void FindSum(int[]array)
+{
+        
+    
+    for (int i = 0; i < array.Length; i++)
+     {
+        if (array[i] % 2 != 0)
+        {
+               result += array[i]; 
+        }
+       
+    }
+    Console.WriteLine(result);
+}
+
+
+

c6f902635316a8520906f0d19bb26daeae077a6b
Lena <len.chaika2011@yandex.ru>
2023-01-05 13:28:43 +0300

    zadacha40

--- zadacha40/Program.cs
+++ zadacha40/Program.cs
@@ -1,38 +1,27 @@
 ﻿// Задача 40: Напишите программу, которая принимает на вход 
 // три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
 
-int length = ReadInt("Array length");
-int[] array = GetRandomArray(length, -9, 9);
+int a = ReadInt("Введите длину стороны А");
+int b = ReadInt("Введите длину стороны В");
+int c = ReadInt("Введите длину стороны С");
 
-PrintArray(array);
-Revers(array);
+CheckTriangle(a, b ,c);
 
-Console.WriteLine();
-PrintArray(array);
 
 int ReadInt(string argument)
 {
-    Console.WriteLine($"Input {argument}:");
-    return int.Parse(Console.ReadLine());
+	Console.Write($"Input {argument}: ");
+	return int.Parse(Console.ReadLine()!);
 }
 
-int[] GetRandomArray(int length, int minValue, int maxValue)
+void CheckTriangle(int a, int b, int c)
 {
-    int[] array = new int[length];
-    Random random = new Random();
-
-    for (int i = 0; i < array.Length; i++)
+    if (a + b > c && b + c > a && a + c > b)
     {
-        array[i] = random.Next(minValue, maxValue + 1);
-
+        System.Console.WriteLine("Треугольник с такими сторонами существует");
     }
-    return array;
-}
-
-void Triangle(int[] array)
-{
-    if (array[0] + array[1] > array[2] && array[1] + array[2] > array[0] && array[0] + array[2] > array[1])
+    else
     {
-        Console.Write(" This is Triangle");
+        System.Console.WriteLine("Треугольник с такими сторонами не существует");
     }
 }

ce1efa4ccb384fc148299e0e07366e66db0a00c8
Lena <len.chaika2011@yandex.ru>
2023-01-10 18:08:59 +0300

    zadacha65

---
+++ zadacha65/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значение M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Выполнить с помощью рекурсии
+
+void PrintFromUntil(int m, int n)
+{
+    if (m >= n)
+    {
+        Console.WriteLine(m);
+        return;
+    }
+    Console.Write($"{m}, ");
+    PrintFromUntil(m+1, n);
+}
+PrintFromUntil(1, 5);

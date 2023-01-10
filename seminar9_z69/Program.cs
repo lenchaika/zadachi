f0c41aa50cb84a735103fa2e4be6de47a099f35a
Lena <len.chaika2011@yandex.ru>
2023-01-10 18:10:12 +0300

    seminar9_z69

---
+++ seminar9_z69/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 69: напишите программу кот. на вход принимает числа А и В и возводит число
+//число А в целую степень В с помощью рекурсии
+
+
+int ProductAandB(int A, int B)
+{
+    if (B == 1)
+    return A;
+    else return(A * ProductAandB(A, B-1));
+}
+Console.WriteLine(ProductAandB(2, 4));
+

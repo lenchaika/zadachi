48c14b6660e4e1836f2b8193139a15daea35adb0
Lena <len.chaika2011@yandex.ru>
2023-01-10 18:09:32 +0300

    zadacha67_69sem9

---
+++ zadacha67_69sem9/Program.cs
@@ -0,0 +1,15 @@
+﻿// напишите программу которая будет принимать на вход число и возвращать сумму его цифр
+//453 ->12
+//45->9
+
+int SumNumbers(int number)
+{
+    if (number / 10 == 0)
+    {
+        return number % 10;
+    }
+    return number % 10 + SumNumbers(number/10);
+}
+Console.WriteLine(SumNumbers(451));
+
+
